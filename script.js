12246214dfa9b79cc372558e23395338012b2537
ap23hp <67959896+ap23hp@users.noreply.github.com>
2022-02-20 14:55:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+var x=2000;
+      
+        setTimeout(()=>{
+            document.body.innerHTML=`<h1>10</h1>`
+            setTimeout(()=>{
+                document.body.innerHTML=`<h1>9</h1>`
+                setTimeout(()=>{
+                    document.body.innerHTML=`<h1>8</h1>`
+                    setTimeout(()=>{
+                        document.body.innerHTML=`<h1>7</h1>`
+                        setTimeout(()=>{
+                            document.body.innerHTML=`<h1>6</h1>`
+                            setTimeout(()=>{
+                                document.body.innerHTML=`<h1>5</h1>`
+                                setTimeout(()=>{
+                                    document.body.innerHTML=`<h1>4</h1>`
+                                    setTimeout(()=>{
+                                        document.body.innerHTML=`<h1>3</h1>`
+                                        setTimeout(()=>{
+                                            document.body.innerHTML=`<h1>2</h1>`
+                                            setTimeout(()=>{
+                                                document.body.innerHTML=`<h1>1</h1>`
+                                                setTimeout(()=>{
+                                                    document.body.innerHTML=`<h1>Happy Independence Day</h1>`
+                                                
+                                                },x);
+                                            },x);
+                                        },x);
+                                    },x);
+                                },x);
+                            },x);
+                        },x);
+                    },x);
+                },x);
+            },x);
+        },x);
+       // document.body.innerHTML=`<h1>HappyIndependance day</h1>`
+       
+
+      
+        
+        
